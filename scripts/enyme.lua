37f2b4826bbaf264aa0dcadea31b791fb85ab2db
Lskram <dankflamemasterLove@gmail.com>
2024-07-18 11:14:16 +0700

    fix enemy

--- scripts/enyme.lua
+++ scripts/enyme.lua
@@ -3,12 +3,20 @@ local function newEnemy(x, y)
         x = x,
         y = y,
         speed = 50,
+        step = 50,
+        direction = 1, -- 1 for down, -1 for up
         image = love.graphics.newImage("assests/images/enyme.png")
 
     }
 
     function enemy.update(dt)
-        enemy.y = enemy.y + enemy.speed * dt
+        enemy.y = enemy.y + enemy.speed * dt * enemy.direction
+        -- Change direction when reaching the step
+        if enemy.y >= enemy.step + y then
+            enemy.direction = -1
+        elseif enemy.y <= y then
+            enemy.direction = 1
+        end
     end
     
     function enemy.draw()
